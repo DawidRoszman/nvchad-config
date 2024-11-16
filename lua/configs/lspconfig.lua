b84e239b07722f78bc1bc6d8bd7d190b404585a2
Dawid Roszman <dawidroszman@protonmail.com>
2024-11-16 15:16:12 +0100

    add prettier

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "jdtls", "clangd", "gopls", "templ", "gdscript", "pyright", "angularls" }
+local servers = { "html", "cssls", "jdtls", "clangd", "gopls", "templ", "gdscript", "pyright", "angularls"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
