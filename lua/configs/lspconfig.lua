9210ed57a5b23b52cd5d371d338c6387939cd110
Dawid Roszman <dawidroszman@protonmail.com>
2024-12-11 19:06:56 +0100

    add support for vue files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "jdtls", "clangd", "gopls", "templ", "gdscript", "pyright", "tailwindcss", "eslint", "angularls" }
+local servers = { "html", "cssls", "jdtls", "clangd", "gopls", "templ", "gdscript", "pyright", "tailwindcss", "eslint", "angularls", "volar" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -31,11 +31,36 @@ lspconfig.jdtls.setup {
   }
 }
 -- typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
+
+-- If you are using mason.nvim, you can get the ts_plugin_path like this
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
 lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 }
+
+-- No need to set `hybridMode` to `true` as it's the default value
+lspconfig.volar.setup {}
+
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   on_init = on_init,
