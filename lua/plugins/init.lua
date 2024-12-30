08c02f1c741adf0e8180749748ac11ae2929bc82
Dawid Roszman <dawidroszman@protonmail.com>
2024-12-30 08:30:53 +0100

    remove markview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -96,16 +96,6 @@ return {
   }
 },
 {
-    "OXY2DEV/markview.nvim",
-    lazy = false,      -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
-
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
-    }
-},
-{
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = "cd app && yarn install",
