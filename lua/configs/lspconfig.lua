fc8b4f7e3f4ea51bb2b370cf08073bbbe4e5e402
Dawid Roszman <dawidroszman@protonmail.com>
2025-06-27 08:17:56 +0200

    Update to Mason 2.0

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,24 +1,15 @@
--- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
-local m = require("mason-lspconfig")
-
-m.setup {
-  ensure_installed = {"gopls", "ts_ls"},
+require("mason").setup()
+require("mason-lspconfig").setup {
+  ensure_installed = {
+    "lua_ls",
+  },
 }
 
--- m.setup_handlers {
---   function (server_name)
---     require('lspconfig')[server_name].setup({
---       on_attach = on_attach,
---       on_init = on_init,
---       capabilities = capabilities,
---     })
---   end
--- }
---java
+-- Java (jdtls)
 lspconfig.jdtls.setup {
   on_attach = on_attach,
   on_init = on_init,
@@ -28,28 +19,21 @@ lspconfig.jdtls.setup {
       project = {
         lombok = {
           enabled = true,
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 }
 
--- typescript
--- lspconfig.ts_ls.setup {
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
--- }
-
--- If you are using mason.nvim, you can get the ts_plugin_path like this
--- local mason_registry = require('mason-registry')
--- local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
-
+-- TypeScript (ts_ls)
 lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
 
+  -- If you are using mason.nvim, you can get the ts_plugin_path like this
+  -- local mason_registry = require('mason-registry')
+  -- local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
   -- init_options = {
   --   plugins = {
   --     {
@@ -59,22 +43,47 @@ lspconfig.ts_ls.setup {
   --     },
   --   },
   -- },
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 }
 
--- No need to set `hybridMode` to `true` as it's the default value
-lspconfig.volar.setup {}
+-- Volar (Vue language server)
+-- Note: 'volar' needs to be installed by Mason if you use it directly.
+-- If 'ts_ls' with the Vue plugin is sufficient, you might not need volar.
+-- If you need specific settings for volar, add them here:
+lspconfig.volar.setup {
+  -- on_attach = on_attach,
+  -- on_init = on_init,
+  -- capabilities = capabilities,
+  -- ... other volar specific settings
+}
 
+-- Tailwind CSS
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
-  filetypes = { "html", "jsx", "tsx", "templ" }
+  filetypes = { "html", "jsx", "tsx", "templ" },
 }
-lspconfig.emmet_language_server.setup({
+
+-- Emmet Language Server
+lspconfig.emmet_language_server.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "templ" },
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "typescriptreact",
+    "vue",
+    "templ",
+  },
   init_options = {
     html = {
       options = {
@@ -82,5 +91,5 @@ lspconfig.emmet_language_server.setup({
         ["bem.enabled"] = true,
       },
     },
-  }
-})
+  },
+}
